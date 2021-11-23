5bd3842e042f2fb47342b52482f0dfda871111bc
semenov-lev <firemail.leo@gmail.com>
2021-11-23 16:05:16 +0300

    add MEDIA_URL

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 import mainapp.views as mainapp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.index, name='index'),
     path('products/', mainapp.products, name='products'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
