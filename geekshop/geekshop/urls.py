33432b67d8d647a5d56a44ad0a534522076bb486
semenov-lev <firemail.leo@gmail.com>
2021-11-24 12:20:45 +0300

    add URL dispatcher for namespace mainapp

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,7 +24,7 @@ import mainapp.views as mainapp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.index, name='index'),
-    path('products/', mainapp.products, name='products'),
+    path('products/', include('mainapp.urls', namespace='mainapp')),
 ]
 
 if settings.DEBUG:
