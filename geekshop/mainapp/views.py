0d064c4a013ca321979c2cc4639c142eb9dd9e11
semenov-lev <firemail.leo@gmail.com>
2021-12-12 15:21:21 +0300

    fix title

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,14 +5,14 @@ from .models import Product
 
 def index(request):
     context = {
-        'title': 'geekShop',
+        'title': 'GeekShop',
     }
     return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
     context = {
-        'title': 'geekShop - Каталог',
+        'title': 'GeekShop | Каталог',
         'products': Product.objects.all()[:6]
     }
     return render(request, 'mainapp/products.html', context)
