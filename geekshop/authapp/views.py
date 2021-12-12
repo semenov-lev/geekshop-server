dc5d9293e9a49f0e06e9964b6dab5d6d593fd3e8
semenov-lev <firemail.leo@gmail.com>
2021-12-12 20:44:13 +0300

    add Login and Register forms, auth and valid requests

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render
 
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from authapp.forms import UserLoginForm, UserRegisterForm
+
 # Create your views here.
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'GeekShop | Авторизация',
+        'form': UserLoginForm(),
     }
     return render(request, 'authapp/login.html', context)
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('authapp:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
     context = {
         'title': 'GeekShop | Регистрация',
+        'form': UserRegisterForm(),
     }
     return render(request, 'authapp/register.html', context)
