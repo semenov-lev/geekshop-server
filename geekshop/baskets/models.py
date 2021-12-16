b776f888cd101e5b993975ff8401f8c281549861
semenov-lev <firemail.leo@gmail.com>
2021-12-16 15:13:22 +0300

    add new Model Basket

--- geekshop/baskets/models.py
+++ geekshop/baskets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from mainapp.models import Product
+from authapp.models import User
 
-# Create your models here.
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    update_timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Корзина пользователя '{self.user.username}' с товаром '{self.product.name}'"
