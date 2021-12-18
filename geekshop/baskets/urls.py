a7dcc1a5ee33a5d7c780b3077bcf9c10e3cfdbe5
semenov-lev <firemail.leo@gmail.com>
2021-12-18 18:16:05 +0300

    added ajax for basket

--- geekshop/baskets/urls.py
+++ geekshop/baskets/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import basked_add
-from .views import basket_remove
+from .views import basked_add, basket_remove, basket_edit
 
 app_name = 'baskets'
 
 urlpatterns = [
     path('add/<int:id>/', basked_add, name='basked_add'),
     path('delete/<int:basked_id>/', basket_remove, name='basket_remove'),
+    path('edit/<int:id_basket>/<int:quantity>/', basket_edit, name='basket_edit'),
 ]
--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
+from django.template.loader import render_to_string
+
 from mainapp.models import Product
 from .models import Basket
 
 
+@login_required
 def basked_add(request, id):
     user = request.user
     product = Product.objects.get(id=id)
@@ -17,6 +21,25 @@ def basked_add(request, id):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
+def basket_edit(request, id_basket, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id_basket)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        context = {
+            'baskets': baskets
+        }
+        result = render_to_string('baskets/basket.html', context)
+        return JsonResponse({'result': result})
+
+
+@login_required
 def basket_remove(request, basked_id):
     Basket.objects.get(id=basked_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
