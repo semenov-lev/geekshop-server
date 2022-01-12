6744d34000afc3f5d5eb4333870ebf3df60956ca
semenov-lev <firemail.leo@gmail.com>
2022-01-12 16:16:59 +0300

    add basket (tabular in line) to django.admin

--- geekshop/authapp/admin.py
+++ geekshop/authapp/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+
+from baskets.admin import BasketAdmin
+from baskets.models import Basket
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = Basket
+    inlines = (BasketAdmin,)
--- geekshop/baskets/admin.py
+++ geekshop/baskets/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Basket
 
-admin.site.register(Basket)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+    readonly_fields = ('create_timestamp', 'update_timestamp',)
+    extra = 0
