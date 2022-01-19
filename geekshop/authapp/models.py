a6255b29ffc54ee144fc160285f95b967d9ff53c
semenov-lev <firemail.leo@gmail.com>
2022-01-20 00:37:06 +0300

    added UserProfile model

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
@@ -19,3 +21,26 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.username}'
+
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'Мужчина'),
+        (FEMALE, 'Женщина'),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    about_me = models.TextField(verbose_name='О себе', blank=True, null=True)
+    gender = models.CharField(verbose_name='Пол', choices=GENDER_CHOICES, blank=True, max_length=2)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
