9c706f5f89f191770ebaaf614dc0e52d0993b4d9
semenov-lev <firemail.leo@gmail.com>
2022-01-25 15:15:20 +0300

    added ordersapp Form, Views

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from mainapp.views import ProductDetail, CatalogView
-from ordersapp.views import OrdersListView, OrdersCreateView, OrdersDetailView, OrdersUpdateView, OrdersDeleteView
+from ordersapp.views import OrdersListView, OrdersCreateView, OrdersDetailView, OrdersUpdateView, OrdersDeleteView, \
+    order_forming_complete
 
 app_name = 'ordersapp'
 
 urlpatterns = [
     path('', OrdersListView.as_view(), name='list'),
     path('create/', OrdersCreateView.as_view(), name='create'),
-    path('detail/', OrdersDetailView.as_view(), name='detail'),
-    path('update/', OrdersUpdateView.as_view(), name='update'),
-    path('delete/', OrdersDeleteView.as_view(), name='delete'),
+    path('detail/<int:pk>/', OrdersDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', OrdersUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', OrdersDeleteView.as_view(), name='delete'),
+    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
 ]
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,27 +1,115 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.db import transaction
+from django.forms import inlineformset_factory
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
+from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin
-from ordersapp.models import Order
+from ordersapp.forms import OrderItemsForm
+from ordersapp.models import Order, OrderItem
 
 
 class OrdersListView(ListView, BaseClassContextMixin):
     model = Order
     template_name = 'ordersapp/order_list.html'
-    title = 'Заказы | Список заказов'
+    title = 'GeekShop | Список заказов'
 
 
-class OrdersCreateView(TemplateView):
-    pass
+class OrdersCreateView(CreateView, BaseClassContextMixin):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:list')
+    title = 'GeekShop | Оформить заказ'
+
+    def get_context_data(self, **kwargs):
+        context = super(OrdersCreateView, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemsForm, extra=1)
+
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            basket_item = Basket.objects.filter(user=self.request.user)
+            if basket_item:
+                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemsForm, extra=basket_item.count())
+                formset = OrderFormSet()
+                for count, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_item[count].product
+                    form.initial['quantity'] = basket_item[count].quantity
+                    form.initial['price'] = basket_item[count].product.price
+                    # basket_item.delete()
+            else:
+                formset = OrderFormSet()
+        context['orderitems'] = formset
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if formset.is_valid():
+                formset.instance = self.object
+                formset.save()
+
+        if self.object.get_total_cost == 0:
+            self.object.delete()
+
+        return super(OrdersCreateView, self).form_valid(form)
 
 
-class OrdersDetailView(TemplateView):
-    pass
+class OrdersDetailView(DetailView, BaseClassContextMixin):
+    model = Order
+    title = 'GeekShop | Просмотр заказа'
 
 
-class OrdersUpdateView(TemplateView):
-    pass
+class OrdersUpdateView(UpdateView, BaseClassContextMixin):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:list')
+    title = 'GeekShop | Редактирование заказа'
+
+    def get_context_data(self, **kwargs):
+        context = super(OrdersUpdateView, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemsForm, extra=1)
+
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormSet(instance=self.object)
+            for form in formset:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
+        context['orderitems'] = formset
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['orderitems']
+
+        with transaction.atomic():
+            self.object = form.save()
+            if formset.is_valid():
+                formset.instance = self.object
+                formset.save()
+
+        if self.object.get_total_cost == 0:
+            self.object.delete()
+
+        return super(OrdersUpdateView, self).form_valid(form)
 
 
-class OrdersDeleteView(TemplateView):
-    pass
+class OrdersDeleteView(DeleteView, BaseClassContextMixin):
+    model = Order
+    success_url = reverse_lazy('ordersapp:list')
+    title = 'GeekShop | Удаление заказа'
+
+
+def order_forming_complete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order.status = Order.SEND_TO_PROCEED
+    order.save()
+    return HttpResponseRedirect(reverse('ordersapp:list'))
